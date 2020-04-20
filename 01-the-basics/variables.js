96f9b6c9f5bd3d60182fef233cade33d7b7e71c1
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-04-20 21:29:32 +0800

    Update section 1 files

---
+++ 01-the-basics/variables.js
@@ -0,0 +1,21 @@
+// 'use strict';
+const first = 'Genesis';
+const age = 27;
+const cool = true;
+
+// first = 'gen'
+// cool = false;
+
+// var dog;
+// dog = 'snickers';
+
+const Dog = 'bowser';
+
+// camelCase
+const iLovePizza = true;
+// UpperCamelCase or PascalCase
+const ILoveToEatHotDogs = false;
+// snake_case
+const this_is_snake_case = 'cool';
+// kebab-case which is not allowed in javaScript
+// let this-is-kebab-case = 'cool';
